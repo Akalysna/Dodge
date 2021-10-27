237f8e034e50d5b9b47e42f1cf594bec887a3efb
Llona <llona.andre--augustine@etu.unice.fr>
2021-10-27 21:17:30 +0200

    Création des classes permettant d'afficher une fenetre

---
+++ src/main/java/app/Dodge.java
@@ -0,0 +1,18 @@
+package app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Dodge extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		Dodge.launch(args);
+	}
+
+}
